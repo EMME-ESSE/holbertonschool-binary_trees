78c6f14869cbc6927bbfece53e6aad3df94c8bd0
EMME-ESSE <matimancini@gmail.com>
2023-01-11 12:59:04 -0800

    Hight of the tree

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - function that measures the height of a binary tree.
+ * @tree: pointer to the root node of the tree to measure the height.
+ * Return: plus the height of the tree.
+**/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height;
+	size_t right_height;
+
+	if (!tree)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+/**
+ * Forma abreviada de escribir un if-else que compara las alturas izquierda y
+ * derecha y devuelve el valor más alto.
+ * El símbolo "?" es la condición (si left_height es mayor que right_height)
+ * El símbolo ":" representa el Else (right_height mayor que left_height)
+ * se añade "1" para contar la raíz del árbol en el total.
+**/
+	return ((left_height > right_height ? left_height : right_height) + 1);
+}
