30ed22c6611893707de8f5467cd95af42f34c08c
Lauro <laurolamas@gmail.com>
2023-01-15 22:05:22 -0300

    Fix16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -12,8 +12,8 @@ size_t height(const binary_tree_t *tree)
 	if (!tree || (!tree->left && !tree->right))
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	left_height = height(tree->left);
+	right_height = height(tree->right);
 
 	return ((left_height > right_height ? left_height : right_height) + 1);
 }
